c008e879c93453108c52d843742f9c17e88153eb
max anselmo <itmanselmo@gmail.com>
2021-09-27 10:16:54 -0400

    completed assignment, using either a loop or build in string functions

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // using the built in string functions
+  let reverseWord = word.split('').reverse().join('');
+
+  // if you want to use a loop do it like this
+  // for(let i=word.length-1; i>=0; i--){
+  //   reverseWord += word[i];
+  // }
+  console.log(reverseWord);
+
+  if(reverseWord === word) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+    - retrieve passed in word
+    - split word 
+    - reverse split word and recombine
+    - compare to origional input word
 */
 
 /*
